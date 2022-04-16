aeb04807514b889c202d6fa41b1df5f7a260b284
João Vitor Fodão Oliveira Mamede <joaovitoroliveiramamede333@gmail.com>
2022-04-16 16:16:56 -0300

    Estrutura Inicial

---
+++ backend/src/main/java/com/paddon/rowsys/RowsysApplication.java
@@ -0,0 +1,13 @@
+package com.paddon.rowsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RowsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RowsysApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/paddon/rowsys/RowsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paddon.rowsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RowsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
